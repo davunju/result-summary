eccd93d060123dc772ea48f39672208b6d0b004d
David L. Enock <enockdavid55@gmail.com>
2024-01-22 08:48:54 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,73 @@
+import iconMemory from "./assets/icon-memory.svg";
+import iconReaction from "./assets/icon-reaction.svg";
+import iconVerbal from "./assets/icon-verbal.svg";
+import iconVisual from "./assets/icon-visual.svg";
+
+export default function App() {
+  return (
+    <div>
+      <div className="flex bg-white shadow-2xl max-w-[30rem] mx-auto my-[9rem] rounded-2xl max-[500px]:flex-col max-[500px]:max-w-[300px] max-[500px]:my-[1rem]">
+        <section className="rounded-3xl text-white p-6 text-center bg-gradient-to-b from-background1 to-background2">
+          <h1 className="opacity-80 mb-3 text-slate-200 font-semibold">
+            Your Result
+          </h1>
+          <div className="bg-gradient-to-b from-circle1 to-circle2 flex flex-col justify-center items-center mb-3 rounded-full w-fit px-9 py-6 mx-auto">
+            <span className="text-5xl font-semibold">76</span>
+            <span className="opacity-45">of 100</span>
+          </div>
+          <h1 className="font-semibold text-xl mb-3">Great</h1>
+          <p className="text-center text-sm opacity-80 text-slate-200">
+            You scored higher than 65% of the people who have taken these tests.
+          </p>
+        </section>
+        <section className="p-6">
+          <h1 className="font-semibold text-DarkGrayBlue mb-5">Summary</h1>
+          <div className="min-[501px]:w-[12rem] flex flex-col gap-4 text-sm">
+            <div className="flex items-center justify-between bg-lightRed p-1 rounded">
+              <span className="flex items-center text-lightRedText">
+                <img src={iconReaction} className="mr-2 size-[1rem]" />
+                Reaction
+              </span>
+              <span className="text-DarkGrayBlue">
+                <b>80</b> / 100
+              </span>
+            </div>
+            <div className="flex items-center justify-between bg-orangeyYellow p-1 rounded">
+              <span className="flex items-center text-orangeyYellowText">
+                <img src={iconMemory} className="mr-2 size-[1rem]" />
+                Memory
+              </span>
+              <span className="text-DarkGrayBlue">
+                <b>92</b> / 100
+              </span>
+            </div>
+            <div className="flex items-center justify-between bg-greenTeal p-1 rounded">
+              <span className="flex items-center text-greenTealText">
+                <img src={iconVerbal} className="mr-2 size-[1rem]" />
+                Verbal
+              </span>
+              <span className="text-DarkGrayBlue">
+                <b>61</b> / 100
+              </span>
+            </div>
+            <div className="flex items-center justify-between bg-cobaltBlue p-1 rounded mb-2">
+              <span className="flex items-center text-cobaltBlueTxt">
+                <img src={iconVisual} className="mr-2 size-[1rem]" />
+                Visual
+              </span>
+              <span className="text-DarkGrayBlue">
+                <b>72</b> / 100
+              </span>
+            </div>
+            <button
+              className="bg-DarkGrayBlue rounded-full p-[6px] text-white text-sm hover:bg-gradient-to-r from-background1 via-circle1 to-background2"
+              type="button"
+            >
+              Continue
+            </button>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        background1: "hsl(252, 100%, 67%)",
+        background2: "hsl(241, 81%, 54%)",
+        circle1: "hsla(256, 72%, 46%, 1)",
+        circle2: "hsla(241, 72%, 46%, 0)",
+        lightRed: "hsla(0, 100%, 67%, 0.1)",
+        orangeyYellow: "hsla(39, 100%, 56%, 0.1)",
+        greenTeal: "hsla(166, 100%, 37%, 0.1)",
+        cobaltBlue: "hsla(234, 85%, 45%, 0.1)",
+        lightRedText: "hsl(0, 100%, 67%)",
+        orangeyYellowText: "hsl(39, 100%, 56%)",
+        greenTealText: "hsl(166, 100%, 37%)",
+        cobaltBlueTxt: "hsl(234, 85%, 45%)",
+        White: "hsl(0, 0%, 100%)",
+        PaleBlue: "hsl(221, 100%, 96%)",
+        LightLavender: "hsl(241, 100%, 89%)",
+        DarkGrayBlue: "hsl(224, 30%, 27%)",
+      },
+      gradientColorStops: {
+        background: "(hsl(252, 100%, 67%), hsl(241, 81%, 54%))",
+      },
+      screens: {
+        mobile: "375px",
+      },
+    },
+  },
+  plugins: [],
+};
